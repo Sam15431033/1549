58eeae0a8ecbde071ef03170637c75aaae0eb59a
qianfeng <qianfeng@hehehe.local>
2015-11-23 14:48:53 +0800

    ok1

---
+++ Ok/Ok/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ok
+//
+//  Created by qianfeng on 15/11/23.
+//  Copyright (c) 2015年 Sam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
